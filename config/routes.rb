33ecec5551298cc63912e6328cbf05374bbe6fd1
julien <ju.jaffre@gmail.com>
2020-02-18 12:14:20 +0000

    db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #read all tasks
+  get "tasks" => "tasks#index"
+
+  #read one task
+  get "tasks/id" => "tasks#show"
+
+  #create on task(2steps)
+  get "tasks/new" => "tasks#new"
+  post "restaurants" => "restaurants#create"
+
+  #update on task(2steps)
+  get "tasks/:id/edit" => "tasks#edit"
+  patch "restaurants/:id" => "tasks#update"
+
+  #delete on task
+  delete "tasks/:id" => "tasks#destroy"
 end
